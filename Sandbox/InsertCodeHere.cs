a5abf2d6a4cbccc6c9c387ed74145c56b63e4288
Poul Henriksen <pohe@easj.dk>
2021-11-03 09:31:55 +0100

    tilføjet try block

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,18 +7,26 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            try
+            {
+                string input = "";
+                Console.WriteLine("Indtast tal1:");
+                input = Console.ReadLine();
+                int tal1 = int.Parse(input);
 
-            string input = "";
-            Console.WriteLine("Indtast tal1:");
-            input = Console.ReadLine();
-            int tal1 = int.Parse(input);
+                Console.WriteLine("Indtast tal2:");
+                input = Console.ReadLine();
+                int tal2 = int.Parse(input);
 
-            Console.WriteLine("Indtast tal2:");
-            input = Console.ReadLine();
-            int tal2 = int.Parse(input);
-
-            int result = tal1 / tal2;
-            Console.WriteLine($"Resultatet af {tal1}/{tal2} er {result}");
+                int result = tal1 / tal2;
+                Console.WriteLine($"Resultatet af {tal1}/{tal2} er {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+           
 
             //Console.WriteLine("Hello world!");
 
